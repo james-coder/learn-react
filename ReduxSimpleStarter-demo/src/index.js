6370d3ad58f388eeea2ba0a988a704fd403463ca
linagee <linagee@gmail.com>
2018-09-09 12:54:36 -0600

    Part 2 of lesson

--- ReduxSimpleStarter-demo/src/index.js
+++ ReduxSimpleStarter-demo/src/index.js
@@ -1,24 +1,43 @@
+import _ from 'lodash';
 import React, { Component }  from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyBQqq5G2orohz5fNoH-DFYEU1-Alcl1Xys';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
-    YTSearch({ key: API_KEY, term: 'surfboards' }, videos => {
-      this.setState({ videos });
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term}, videos => {
+      this.setState({
+        videos,
+        selectedVideo: videos[0]
+       });
     });
   }
 
   render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term)}, 300);
+
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
       </div>
     );
   }

a2946233fe4cba217b984c8765a4283bbc6e87a2
linagee <linagee@gmail.com>
2018-09-11 23:28:26 -0600

    Starting next lesson, line graphs

--- ReduxSimpleStarter-demo2/src/reducers/reducer_books.js
+++ ReduxSimpleStarter-demo2/src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    { title: 'Javascript: The Good'},
-    { title: 'Harry Potter'},
-    { title: 'The Dark Tower'},
-    { title: 'Eloquent Ruy'}
+    { title: 'Javascript: The Good', pages: 101 },
+    { title: 'Harry Potter', pages: 39 },
+    { title: 'The Dark Tower', pages: 85 },
+    { title: 'Eloquent Ruy', pages: 1 }
   ]
 }
